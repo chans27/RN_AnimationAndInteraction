e26aab3eb0e67dfc1debe6064bda6e98ef87287b
YEACHAN <everever1275@gmail.com>
2023-01-09 18:03:29 +0900

    4.16

--- App.js
+++ App.js
@@ -1,67 +1,142 @@
-import React, { useRef } from "react";
-import {Animated, Dimensions, PanResponder} from "react-native";
+import React, { useRef, useState } from "react";
+import { Animated, PanResponder, Text, View } from "react-native";
 import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
+import icons from "./icons";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background-color: #00a8ff;
 `;
-const Box = styled.View`
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
-`;
-const AnimatedBox = Animated.createAnimatedComponent(Box);
 
-const { width:SCREEN_WIDTH, height:SCREEN_HEIGHT } = Dimensions.get("window");
+const Card = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
+  width: 300px;
+  height: 300px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
+  position: absolute;
+`;
+
+const Btn = styled.TouchableOpacity`
+  margin: 0px 10px;
+`;
+
+const BtnContainer = styled.View`
+  flex-direction: row;
+  flex: 1;
+`;
+
+const CardContainer = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function App() {
-  const POSITION = useRef(
-    new Animated.ValueXY({
-      x: 0,
-      y: 0,
+  // Values
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.Value(0)).current;
+  const rotation = position.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ["-15deg", "15deg"],
+  });
+  const secondScale = position.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0.7, 1],
+    extrapolate: "clamp",
+  });
+  // Animations
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.95,
+    useNativeDriver: true,
+  });
+  const goCenter = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
+  const goLeft = Animated.spring(position, {
+    toValue: -500,
+    tension: 5,
+    useNativeDriver: true,
+    restDisplacementThreshold: 500,
+    restSpeedThreshold: 500,
+  });
+  const goRight = Animated.spring(position, {
+    toValue: 500,
+    tension: 5,
+    useNativeDriver: true,
+    restDisplacementThreshold: 500,
+    restSpeedThreshold: 500,
+  });
+  // Pan Responders
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx }) => {
+        position.setValue(dx);
+      },
+      onPanResponderGrant: () => onPressIn.start(),
+      onPanResponderRelease: (_, { dx }) => {
+        if (dx < -120) {
+          goLeft.start(onDismiss);
+        } else if (dx > 120) {
+          goRight.start(onDismiss);
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
+      },
     })
   ).current;
-
-  const borderRadius = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: [100, 0],
-  });
-  const bgColor = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"],
-  });
-  const panResponder = useRef(PanResponder.create({
-    onStartShouldSetPanResponder: () => true,
-    onPanResponderGrant: () => {
-      POSITION.setOffset({
-        x: POSITION.x._value, y: POSITION.y._value
-      });
-    },
-    onPanResponderMove: (_, {dx, dy}) => {
-      POSITION.setValue({
-        x: dx,
-        y: dy,
-      })
-    },
-    onPanResponderRelease: () => {
-      POSITION.flattenOffset();
-    }
-  })).current;
-
+  // State
+  const [index, setIndex] = useState(0);
+  const onDismiss = () => {
+    scale.setValue(1);
+    setIndex((prev) => prev + 1);
+    position.setValue(0);
+    // Animated.timing(position, { toValue: 0, useNativeDriver: true }).start();
+  };
+  const closePress = () => {
+    goLeft.start(onDismiss);
+  };
+  const checkPress = () => {
+    goRight.start(onDismiss);
+  };
   return (
     <Container>
-        <AnimatedBox
+      <CardContainer>
+        <Card style={{ transform: [{ scale: secondScale }] }}>
+          <Ionicons name={icons[index + 1]} color="#192a56" size={98} />
+        </Card>
+        <Card
           {...panResponder.panHandlers}
           style={{
-            borderRadius,
-            backgroundColor: bgColor,
             transform: [
-              ...POSITION.getTranslateTransform(),
+              { scale },
+              { translateX: position },
+              { rotateZ: rotation },
             ],
           }}
-        />
+        >
+          <Ionicons name={icons[index]} color="#192a56" size={98} />
+        </Card>
+      </CardContainer>
+      <BtnContainer>
+        <Btn onPress={closePress}>
+          <Ionicons name="close-circle" color="white" size={58} />
+        </Btn>
+        <Btn onPress={checkPress}>
+          <Ionicons name="checkmark-circle" color="white" size={58} />
+        </Btn>
+      </BtnContainer>
     </Container>
   );
 }
